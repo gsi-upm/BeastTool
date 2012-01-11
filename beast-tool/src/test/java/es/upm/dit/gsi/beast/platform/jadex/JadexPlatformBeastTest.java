1cf2dac4d52a80d2444279d89b88220286b12c73
jjsolitario <jjsolitario@b1f9e93e-674b-0a9e-b090-602bc7d8f047>
2012-01-11 17:24:12 +0000

    No RuntimeException
    git-svn-id: https://beast-tool.googlecode.com/svn/trunk@249 b1f9e93e-674b-0a9e-b090-602bc7d8f047

--- beast-tool/src/test/java/es/upm/dit/gsi/beast/platform/jadex/JadexPlatformBeastTest.java
+++ beast-tool/src/test/java/es/upm/dit/gsi/beast/platform/jadex/JadexPlatformBeastTest.java
@@ -21,46 +21,55 @@ public class JadexPlatformBeastTest {
     @Test
     public void getGoalsFromAgent() {
 
-        Logger logger = Logger.getLogger(JadexPlatformBeastTest.class.getName());
-        JadexConnector connector = (JadexConnector) PlatformSelector.getConnector("jadex", logger);
-        JadexAgentIntrospector introspector = (JadexAgentIntrospector) PlatformSelector.getAgentIntrospector("jadex");
-        connector.launchPlatform();
-        connector.createAgent("Tester", "es/upm/dit/gsi/beast/test/agent/jadex/TesterAgent.agent.xml");
+        try {
+            Logger logger = Logger.getLogger(JadexPlatformBeastTest.class.getName());
+            JadexConnector connector = (JadexConnector) PlatformSelector.getConnector("jadex", logger);
+            JadexAgentIntrospector introspector = (JadexAgentIntrospector) PlatformSelector.getAgentIntrospector("jadex");
+            connector.launchPlatform();
+            connector.createAgent("Tester", "es/upm/dit/gsi/beast/test/agent/jadex/TesterAgent.agent.xml");
 
-        boolean finish = false;
-        IGoal[]  value = null;
-        while (!finish) {
-            value = introspector.getAgentGoals("Tester", connector);
-             if (value.length>0) {
-                 finish = true;
-             }
+            boolean finish = false;
+            IGoal[]  value = null;
+            while (!finish) {
+                value = introspector.getAgentGoals("Tester", connector);
+                 if (value.length>0) {
+                     finish = true;
+                 }
+            }
+            Assert.assertTrue(value instanceof IGoal[]);
+            Assert.assertNotNull(value[0]);
+            Assert.assertEquals(value[0].getType(), "live");
+        } catch (RuntimeException e) {
+            getGoalsFromAgent();
         }
-        Assert.assertTrue(value instanceof IGoal[]);
-        Assert.assertNotNull(value[0]);
-        Assert.assertEquals(value[0].getType(), "live");
     }
     
     
     @Test
     public void getPlansFromAgent() {
 
-        Logger logger = Logger.getLogger(JadexPlatformBeastTest.class.getName());
-        JadexConnector connector = (JadexConnector) PlatformSelector.getConnector("jadex", logger);
-        JadexAgentIntrospector introspector = (JadexAgentIntrospector) PlatformSelector.getAgentIntrospector("jadex");
-        connector.launchPlatform();
-        connector.createAgent("Tester", "es/upm/dit/gsi/beast/test/agent/jadex/TesterAgent.agent.xml");
+        try {
 
-        boolean finish = false;
-        IPlan[] value = null;
-        while (!finish) {
-             value = introspector.getAgentPlans("Tester", connector);
-             if (value.length>0) {
-                 finish = true;
-             }
+            Logger logger = Logger.getLogger(JadexPlatformBeastTest.class.getName());
+            JadexConnector connector = (JadexConnector) PlatformSelector.getConnector("jadex", logger);
+            JadexAgentIntrospector introspector = (JadexAgentIntrospector) PlatformSelector.getAgentIntrospector("jadex");
+            connector.launchPlatform();
+            connector.createAgent("Tester", "es/upm/dit/gsi/beast/test/agent/jadex/TesterAgent.agent.xml");
+
+            boolean finish = false;
+            IPlan[] value = null;
+            while (!finish) {
+                 value = introspector.getAgentPlans("Tester", connector);
+                 if (value.length>0) {
+                     finish = true;
+                 }
+            }
+            Assert.assertTrue(value instanceof IPlan[]);
+            Assert.assertTrue(value.length>0);
+            Assert.assertEquals(value[0].getType(), "survive");
+        } catch (RuntimeException e ) {
+            getPlansFromAgent();
         }
-        Assert.assertTrue(value instanceof IPlan[]);
-        Assert.assertTrue(value.length>0);
-        Assert.assertEquals(value[0].getType(), "survive");
     }
 
     
